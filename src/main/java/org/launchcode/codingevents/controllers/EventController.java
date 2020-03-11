b70231ab294e353e622ca04aa2a930f1ffc685d7
Katherine Ross <katylahv@gmail.com>
2020-03-10 23:26:03 -0500

    exercises completed

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -5,7 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -17,11 +19,10 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
+        HashMap<String,String> events = new HashMap<>();
+        events.put("Code til Dawn", "whatever they do there");
+        events.put("Hackathon", "some stuff");
+        events.put("CoderGirl", "girls coding");
         model.addAttribute("events", events);
         return "events/index";
     }
