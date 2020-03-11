9ade1a4d9add64bc4b577036f1417a8c96a677cb
Katherine Ross <katylahv@gmail.com>
2020-03-10 21:56:44 -0500

    code-along complete

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -16,26 +16,21 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
-
     private static List<String> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
-
+    // lives at /events/create
     @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Event");
+    public String renderCreateEventForm(){
         return "events/create";
     }
-
+    //lives at /events/create
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
+    public String createEvent(@RequestParam String eventName){
         events.add(eventName);
-        return "redirect:";
+        return "redirect:"; // go to diff page
     }
-
 }
